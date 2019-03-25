e66544ff7ed0b4ba66775e3b30c33a677c78be6c
Bulychev Ivan <dstrmv@outlook.com>
2019-03-25 17:45:52 +0400

    lb8fixes

--- src/lab8/Matrix.java
+++ src/lab8/Matrix.java
@@ -12,20 +12,14 @@ public class Matrix {
 
         //не работает ничего
 
-        int m = 6;
         int n = 4;
+        int m = 4;
 
-        int numDims = 1;
         double[][] a = new double[n][m];
         double[][] b = new double[m][n];
-        double[][] c = new double[n][m];
+        double[][] c = new double[n][n];
 
-        int rank, size, d;
-        int[] dims = new int[numDims];
-        int[] newCoords = new int[numDims];
-        boolean[] periods = {true};
-        boolean reorder = false;
-        Cartcomm cart;
+        int rank, size;
 
         MPI.Init(args);
 
@@ -42,33 +36,56 @@ public class Matrix {
                 }
             }
 
+//            a[0][0] = 1;
+//            a[0][1] = 2;
+//            a[0][2] = 3;
+//            a[0][3] = 4;
+//            a[1][0] = 5;
+//            a[1][1] = 6;
+//            a[1][2] = 7;
+//            a[1][3] = 8;
+//
+//            b[0][0] = 9;
+//            b[0][1] = 10;
+//            b[1][0] = 11;
+//            b[1][1] = 12;
+//            b[2][0] = 13;
+//            b[2][1] = 14;
+//            b[3][0] = 15;
+//            b[3][1] = 16;
         }
 
         MPI.COMM_WORLD.Bcast(a, 0, 1, MPI.OBJECT, 0);
         MPI.COMM_WORLD.Bcast(b, 0, 1, MPI.OBJECT, 0);
 
-        Cartcomm.Dims_create(size, dims);
-        cart = MPI.COMM_WORLD.Create_cart(dims, periods, reorder);
+        //int start = rank * (n / size);
+        //int end = (rank + 1) * (n / size);
 
-        newCoords = cart.Coords(rank);
-
-        ShiftParms shift = cart.Shift(0, -1);
-
-
-        for (int k = 0; k < size; k++) {
-            d = k;
+        for (int i = rank; i < n; i += size) {
             for (int j = 0; j < n; j++) {
-                for (int i1 = 0, j1 = d; j1 < d + n; i1++, j1++) {
-                    for (int i = 0; i < m; i++) {
-                        c[j][j1] += a[j][i] * b[i][i1];
-                    }
+                for (int k = 0; k < m; k++) {
+                    c[i][j] += a[i][k] * b[k][j];
                 }
             }
-
-            //cart.Sendrecv_replace(b, 0, 1, MPI.OBJECT, shift.rank_dest, 12, shift.rank_source, 12);
         }
 
+        MPI.COMM_WORLD.Barrier();
+
+        for (int i = rank; i < n; i += size) {
+            if (rank != 0) {
+                MPI.COMM_WORLD.Send(c[i], 0, c[i].length, MPI.DOUBLE, 0, 0);
+            }
+        }
+
+        MPI.COMM_WORLD.Barrier();
+
         if (rank == 0) {
+
+            for (int i = 0; i < n; i++) {
+                if (i%size == 0) continue;
+                MPI.COMM_WORLD.Recv(c[i], 0, c[i].length, MPI.DOUBLE, i % size, 0);
+            }
+
             for (int i = 0; i < a.length; i++) {
                 System.out.println(Arrays.toString(a[i]));
             }
@@ -83,10 +100,8 @@ public class Matrix {
             }
             System.out.println();
 
-
         }
 
-        cart.Free();
         MPI.Finalize();
 
     }
